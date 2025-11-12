8d776bb4dba9de0714f34858c7bd28fdc2f82c25
Kaeden-D <dalyk3@my.erau.edu>
2025-11-12 10:32:58 -0700

    add PlayerController Script

---
+++ Intro to Persistence/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rig;
+    private float startTime;
+    private float timeTaken;
+    private int collectablesPicked;
+    public int maxCollectables = 10;
+    private bool isPlaying;
+
+    void Awake()
+    {
+
+        rig = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rig.linearVelocity = new Vector3(x, rig.linearVelocity.y, z);
+
+    }
+}
